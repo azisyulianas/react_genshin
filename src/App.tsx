7fd92353efe6952e3887c8dab8f5f696cfac7e20
azisyulianas <azis3111@gmail.com>
2025-06-01 14:39:15 +0700

    Add dependencies and update App component with navigation
    - Added axios, genshin-db, and react-router-dom to dependencies in package.json and package-lock.json.
    - Refactored App component to include a responsive navigation bar with Tailwind CSS.
    - Created SvgList component for SVG icons and TailwindLogo component for logo rendering.
    - Removed unused code and styles from App.css.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
+import SvgList from './assets/SvgList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className=''>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className='flex lg:items-center items-start justify-between flex-wrap bg-teal-500 p-3'>
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <SvgList>
+            <SvgList.TailwindLogo />
+          </SvgList>
+          <span className="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+        </div>
+        <div className="rounded-md lg:hidden">
+          <button className={`justify-end px-4 py-2 text-sm font-medium leading-5 ${!isOpen?'hidden':''} `} onClick={toggleDropdown}
+          type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
+            <SvgList>
+              <SvgList.HambugerOpen />
+            </SvgList>
+          </button>
+          <button className={`justify-end px-4 py-2 text-sm font-medium leading-5 ${!isOpen?'':'hidden'}`} onClick={toggleDropdown}
+          type="button" aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
+            <SvgList>
+              <SvgList.HambugerClose />
+            </SvgList>
+          </button>
+        </div>
+        <div className={`lg:flex-grow lg:items-center lg:w-auto lg:flex w-full lg:justify-start ${!isOpen?'hidden':''} `}>
+          <ul className="text-sm lg:flex-grow lg:flex justify-center lg:justify-start text-center">
+            <li className='class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-4'>Characters</li>
+            <li className='class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-4'>Weapons</li>
+            <li className='class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-4'>Materials</li>
+            <li className='class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-4'>Enemies</li>
+            <li className='class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-4'>Artifact</li>
+            <li className='class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white lg:mr-4'>Info</li>
+          </ul>
+        </div>
+      </nav>
     </>
   )
 }
---
+++ src/assets/SvgList.tsx
@@ -0,0 +1,33 @@
+const SvgList = (props:{children:React.ReactNode}) => {
+  const { children="" } = props
+  return <>{children}</>
+}
+
+const TailwindLogo = () =>{
+  return (
+    <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+  )
+}
+
+const HambugerOpen = () => {
+  return (
+    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <title>Menu</title>
+      <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+    </svg>
+  )
+}
+
+const HambugerClose = () => {
+  return (
+    <svg className="fill-current h-3 w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/>
+    </svg>
+  )
+}
+
+SvgList.TailwindLogo = TailwindLogo
+SvgList.HambugerOpen = HambugerOpen
+SvgList.HambugerClose = HambugerClose
+
+export default SvgList
